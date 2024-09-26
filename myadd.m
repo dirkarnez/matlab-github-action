c4fa62a1e14593f36bec8ff3d8b09e3bbd4914ef
Dirk Arnez <smalldirkalex@gmail.com>
2024-09-26 13:07:58 +0800

    - update myadd.m

--- myadd.m
+++ myadd.m
@@ -1,3 +1,12 @@
+cfg = coder.config("lib");
+cfg.TargetLang = "C++";
+
+cfg.CustomSource = "main.cpp";
+cfg.CustomInclude = {pwd};
+
+cfg.GenCodeOnly = true;
+cfg.Toolchain = "CMake";
+
 function [c] = myadd(a,b)
   c = a + b;
 end
